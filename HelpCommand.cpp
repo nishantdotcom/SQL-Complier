5e5e1c8070bf826b977c43c9adc00daa251f2991
nishantkumar1308 <nishantsingh1308@gmail.com>
2023-03-15 13:59:45 +0530

    Creating Help commands

--- HelpCommand.cpp
+++ HelpCommand.cpp
@@ -28,3 +28,43 @@ void HelpTables()
 	}
 }
 
+
+
+
+void HelpCommand(vector<string> Tokens)
+{
+	if(Tokens[1]=="Create")
+	{
+		cout<<"Used to create tables in SQL"<<endl;
+        cout<<"SYNTAX : ";
+        cout<<"create table table_name (attr1 type1, attr2 type2 check(cond1 AND cond2), primary key(attr1));"<<endl;
+        cout<<"Eg -"<<endl;
+        cout<<"create table Student(roll int check(roll>0), name varchar(20) check(name!=\"ABC\"), dob date, percent decimal(4,2), primary key(roll));";
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
